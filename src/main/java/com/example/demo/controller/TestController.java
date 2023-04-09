128d30b44552052fefc3ec3839115743e8151e87
raylei <rayleiHins456>
2023-04-09 11:55:36 +0800

    test

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * something to do
+ *
+ * @author Ray lei
+ * @date 2023/4/9 11:54
+ */
+@RestController("test")
+public class TestController {
+
+
+    @GetMapping("getTesting")
+    public Object getTesting() {
+        return 1;
+    }
+}
